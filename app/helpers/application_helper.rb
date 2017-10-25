7fd798bf4e402db49b85b1099ed5cf417914d96a
Brandon Valle <bjvtamayo78@gmail.com>
2017-10-25 19:03:20 -0400

    update menu_bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+	def parent_menu_active(controller, menu)
+		if controller == "home" && menu == "dashboard"
+			return "active open selected"
+		elsif (controller == "type_of_products" || controller == "products" || controller == "inflows" ) && menu == "product"
+			return "active open selected"
+		end
+	end
+
+	def active_menu(controller, controller_inside)
+		if controller == controller_inside
+			"active"
+		else
+			""
+		end
+	end
 end
