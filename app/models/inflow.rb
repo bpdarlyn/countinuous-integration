09539f6ec4808ebb15e035019820d7032df880bd
Brandon Valle <bjvtamayo78@gmail.com>
2017-10-26 11:15:49 -0400

    fix on delete inflows with relations

--- app/models/inflow.rb
+++ app/models/inflow.rb
@@ -1,4 +1,4 @@
 class Inflow < ApplicationRecord
-	has_many :inflow_details, inverse_of: :inflow
+	has_many :inflow_details, inverse_of: :inflow, dependent: :destroy
 	accepts_nested_attributes_for :inflow_details, reject_if: :all_blank, allow_destroy: true
 end
