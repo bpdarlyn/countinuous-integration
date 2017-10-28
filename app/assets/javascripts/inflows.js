f8e73516faf4b9b57e3cf142f96244bc48354ab8
Brandon Jason <bjvtamayo78@gmail.com>
2017-10-27 23:42:15 -0400

    update quantity in orden request

--- app/assets/javascripts/inflows.js
+++ app/assets/javascripts/inflows.js
@@ -2,18 +2,14 @@ $("#add-item").click(function(){
  	$('#cocconAddItem').trigger('click');
 });
 
-
 $(document).on('change', "input[name*='[inflow_details_attributes]'][name*='[start_number]']", function () {
-	console.log("Hola");
     var input = $(this).parents('tr').find("input[name*='[inflow_details_attributes]'][name*='[quantity]']");
     var end_number = $(this).parents('tr').find("input[name*='[inflow_details_attributes]'][name*='[end_number]']").val();
     input.val(end_number - $(this).val() + 1);
     input.change();
 });
 
-
 $(document).on('change', "input[name*='[inflow_details_attributes]'][name*='[end_number]']", function () {
-	console.log("Cambio el end number")
     var input = $(this).parents('tr').find("input[name*='[inflow_details_attributes]'][name*='[quantity]']");
     var start_number = $(this).parents('tr').find("input[name*='[inflow_details_attributes]'][name*='[start_number]']").val();
     input.val($(this).val() - start_number + 1);
--- app/assets/javascripts/orden_requests.js
+++ app/assets/javascripts/orden_requests.js
@@ -1,3 +1,27 @@
 $("#add-item-orden-request").click(function(){
  	$('#cocconAddItem').trigger('click');
 });
+
+$(document).on('change', "input[name*='[orden_request_details_attributes]'][name*='[quantity]']", function () {
+    var total_quantity = 0;
+    $("input[name*='[orden_request_details_attributes]'][name*='[quantity]']")
+      .each(function(){
+        total_quantity += parseInt($(this).val());
+      });
+    var input_total_quantity = $("#orden_request_ticket_quantity");
+    input_total_quantity.val(total_quantity);
+});
+
+
+$(document).on('click',".remove-orden-request-field", function(){
+  var input = $(this).parents('tr').find("input[name*='[orden_request_details_attributes]'][name*='[quantity]']");
+  var total_quantity = 0;
+  $("input[name*='[orden_request_details_attributes]'][name*='[quantity]']")
+    .each(function(){
+      total_quantity += parseInt($(this).val());
+    });
+
+  total_quantity -= parseInt(input.val());
+  var input_total_quantity = $("#orden_request_ticket_quantity");
+  input_total_quantity.val(total_quantity);
+})
