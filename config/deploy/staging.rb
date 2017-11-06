fd551e0fc4ec9533c5aa1d2b7af04ce456f94676
bpdarlyn <bpdarlyn@gmail.com>
2017-11-06 14:37:14 -0400

    staging :)

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -69,8 +69,8 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
+      unless `git rev-parse HEAD` == `git rev-parse staging`
+        puts "WARNING: HEAD is not the same as staging"
         puts "Run `git push` to sync changes."
         exit
       end
